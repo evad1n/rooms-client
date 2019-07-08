70cb68a14d1c73e5bd99dd80871ae944a946df48
evad1n <willdickinson98@gmail.com>
2019-07-08 11:16:03 -0600

    stuff

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-const url = 'https://server-rooms.herokuapp.com'
-//const url = 'http://localhost:3000'
+//const url = 'https://server-rooms.herokuapp.com'
+const url = 'http://localhost:3000'
 
 var app = new Vue({
     el: "#app",
 
     data: {
-        page: "messages",
+        page: "test",
         rooms: [
             "messages",
             "test"
         ],
         drawer: false,
         newMessage: "",
-        messageHistory: []
+        messageHistory: [],
+        position: {}
     },
 
     created: function () {
@@ -28,20 +29,46 @@ var app = new Vue({
             fetch(`${url}/messaging`).then(function (res) {
                 res.json().then(function (data) {
                     app.messageHistory = data.history;
+                    app.position = data.position;
                 });
             });
         },
         send: function () {
-            fetch(`${url}/messaging`, {
+            if (this.newMessage != "") {
+                fetch(`${url}/messaging`, {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify({ message: app.newMessage })
+                }).then(function () {
+                    app.newMessage = ""
+                    app.update()
+                });
+            }
+        },
+        moveLeft: function () {
+            fetch(`${url}/game`, {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json"
                 },
-                body: JSON.stringify({message: app.newMessage})
+                body: JSON.stringify({ move: "left" })
             }).then(function () {
                 app.update()
             });
         },
+        moveRight: function () {
+            fetch(`${url}/game`, {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify({ move: "right" })
+            }).then(function () {
+                app.update()
+            });
+        }
     },
 
     computed: {
