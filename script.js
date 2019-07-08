e4c065614411173d947b900f7c69bc37cd5e747a
evad1n <willdickinson98@gmail.com>
2019-07-08 12:58:55 -0600

    stuff look good

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const url = 'https://server-rooms.herokuapp.com'
 //const url = 'http://localhost:3000'
 
+const UPDATE_INTERVAL = 1000
+
 var app = new Vue({
     el: "#app",
 
@@ -10,30 +12,89 @@ var app = new Vue({
             "messages",
             "test"
         ],
-        drawer: false,
+        interval: 0,
         newMessage: "",
         messageHistory: [],
-        position: {}
+        gameMessageHistory: [],
+        playerID: 0,
+        characters: [],
+        playerColor: "red",
+        color: {
+            "r": 0,
+            "g": 0,
+            "b": 0
+        }
     },
 
     created: function () {
-        var x = 0
-        x = setInterval(() => {
-            x++
-            this.update()
-        }, 1000);
+        fetch(`${url}/game/login`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+        }).then(function (res) {
+            res.json().then(function (data) {
+                app.playerID = data.id
+            });
+        });
+
+        this.interval = setInterval(() => {
+            this.updateGame()
+        }, UPDATE_INTERVAL);
+
+        window.addEventListener("keyup", this.keyEvents);
     },
 
     methods: {
-        update: function () {
+        onLeave: function () {
+            fetch(`${url}/game/${app.playerID}`, {
+                method: "DELETE",
+            })
+            return null
+        },
+        keyEvents: function (e) {
+            if (this.page == "test") {
+                if (e.which == 87) {
+                    this.gameMove("up")
+                } else if (e.which == 65) {
+                    this.gameMove("left")
+                } else if (e.which == 83) {
+                    this.gameMove("down")
+                } else if (e.which == 68) {
+                    this.gameMove("right")
+                }
+            }
+        },
+        goToMessages: function () {
+            this.page = "messages"
+            clearInterval(this.interval)
+            this.interval = setInterval(() => {
+                this.getMessages()
+            }, UPDATE_INTERVAL);
+        },
+        goToTest: function () {
+            this.page = "test"
+            clearInterval(this.interval)
+            this.interval = setInterval(() => {
+                this.updateGame()
+            }, UPDATE_INTERVAL);
+        },
+        getMessages: function () {
             fetch(`${url}/messaging`).then(function (res) {
                 res.json().then(function (data) {
                     app.messageHistory = data.history;
-                    app.position = data.position;
                 });
             });
         },
-        send: function () {
+        updateGame: function () {
+            fetch(`${url}/game`).then(function (res) {
+                res.json().then(function (data) {
+                    app.gameMessageHistory = data.history;
+                    app.characters = data.characters;
+                });
+            });
+        },
+        sendMessage: function () {
             if (this.newMessage != "") {
                 fetch(`${url}/messaging`, {
                     method: "POST",
@@ -43,30 +104,44 @@ var app = new Vue({
                     body: JSON.stringify({ message: app.newMessage })
                 }).then(function () {
                     app.newMessage = ""
-                    app.update()
+                    app.getMessages()
                 });
             }
         },
-        moveLeft: function () {
-            fetch(`${url}/game`, {
+        sendGameMessage: function () {
+            if (this.newMessage != "") {
+                fetch(`${url}/messages/${app.playerID}`, {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify({ message: app.newMessage })
+                }).then(function () {
+                    app.newMessage = ""
+                    app.updateGame()
+                });
+            }
+        },
+        gameMove: function (move) {
+            fetch(`${url}/game/${app.playerID}`, {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json"
                 },
-                body: JSON.stringify({ move: "left" })
+                body: JSON.stringify({ move: move })
             }).then(function () {
-                app.update()
+                app.updateGame()
             });
         },
-        moveRight: function () {
-            fetch(`${url}/game`, {
+        updateColor: function () {
+            fetch(`${url}/game/color/${app.playerID}`, {
                 method: "POST",
                 headers: {
                     "Content-type": "application/json"
                 },
-                body: JSON.stringify({ move: "right" })
+                body: JSON.stringify({ color: color })
             }).then(function () {
-                app.update()
+                app.updateGame()
             });
         }
     },
@@ -75,3 +150,5 @@ var app = new Vue({
 
     }
 })
+
+window.onbeforeunload = app.onLeave;
