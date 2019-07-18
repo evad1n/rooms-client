18ed8249ad77fd8d4cce20af9e2389a972186810
AntoineOMEGA <31717032+AntoineOMEGA@users.noreply.github.com>
2019-07-18 12:33:35 -0600

    Update cubes.js

--- Agario/cubes.js
+++ Agario/cubes.js
@@ -2,13 +2,13 @@ var pUsername = "Antoine";
 var pPosition = { x: 0, y: 0, z: 0 };
 var pRotation = { x: 0, y: 0, z: 0 };
 var pColor = 0xFFFFFF;
-var pAmount = 25;
+var pAmount = 36;
 var pSavedAmount = 0;
+var pAlive = true;
 var pX = 0;
+var pY = 0;
 var pZ = 0;
 var pMesh = null;
-
-var pafjkjaf = 9;
 
 var players = [
     {
@@ -22,21 +22,20 @@ var players = [
         username: "Japmes",
         position: { x: -19, y: 0, z: 0 },
         color: 0x00FF00,
-        amount: 16,
+        amount: 25,
         alive: true
     },
     {
         username: "Jamppes",
         position: { x: 1, y: 0, z: 16 },
         color: 0xFF00FF,
-        amount: 4,
+        amount: 9,
         alive: true
     },
 ];
 
-var objects = [
-
-];
+var asteroidAmount = 1000;
+var asteroids = [];
 
 var w_width = window.innerWidth;
 var w_height = window.innerHeight;
@@ -53,13 +52,13 @@ var spaceArray = [
     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('images/outerspaceBK.png'), side: THREE.BackSide })
 ]
 
-//HI
-
 var spaceGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
 var space = new THREE.Mesh(spaceGeometry, spaceArray);
 scene.add(space);
 
 var camera = new THREE.PerspectiveCamera(75, w_width / w_height, 0.1, 25000);
+var direction = new THREE.Vector3();
+camera.getWorldDirection(direction);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w_width, w_height);
@@ -67,10 +66,8 @@ document.body.appendChild(renderer.domElement);
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enablePan = false;
-controls.minDistance = Math.sqrt(pAmount) * 3;
-controls.maxDistance = Math.sqrt(pAmount) * 5;
-
-
+controls.minDistance = Math.sqrt(pAmount) * 4;
+controls.maxDistance = Math.sqrt(pAmount) * 4;
 
 //EVENT LISTENERS
 window.addEventListener('resize', function () {
@@ -81,13 +78,18 @@ window.addEventListener('resize', function () {
 })
 
 window.addEventListener('keydown', function (e) {
-    if (e.which == 38) {
-        pX += 1;
-    } else if (e.which == 39) {
+    this.console.log(e.which);
+    if (e.which == 87) {
         pX -= 1;
-    } else if (e.which == 37) {
+    } else if (e.which == 83) {
+        pX += 1;
+    } else if (e.which == 32) {
+        pY += 1;
+    } else if (e.which == 67) {
+        pY -= 1;
+    } else if (e.which == 68) {
         pZ += 1;
-    } else if (e.which == 40) {
+    } else if (e.which == 65) {
         pZ -= 1;
     }
 })
@@ -108,6 +110,26 @@ var createAmbientLight = function () {
 var createScene = function () {
     createSun();
     createAmbientLight();
+    for (var i = 0; i < asteroidAmount; i++) {
+        var geometry = new THREE.BoxGeometry(1,1,1);
+        var material = new THREE.MeshLambertMaterial({color: 0xAA5522});
+        var mesh = new THREE.Mesh(geometry,material);
+        mesh.position.set(
+            (Math.random() * 100) - Math.random() * 200,
+            (Math.random() * 100) - Math.random() * 200,
+            (Math.random() * 100) - Math.random() * 200
+            );
+    
+        var amount = Math.random() * 10;
+        var asteroid = {
+            amount: amount,
+            position: mesh.position,
+            alive: true,
+            mesh: mesh
+        };
+        asteroids.push(asteroid);
+        scene.add(asteroid.mesh);
+    }
 }
 
 createScene();
@@ -126,7 +148,7 @@ mtlLoader.load('models/raptor.mtl', function (materials) {
 //UPDATE
 var createPlayer = function () {
     var geometry = new THREE.SphereGeometry(1, 32, 32);
-    var material = new THREE.MeshPhongMaterial({ color: pColor });
+    var material = new THREE.MeshPhongMaterial({ color: pColor, wireframe: true });
     pMesh = new THREE.Mesh(geometry, material);
     pMesh.scale.set(Math.sqrt(pAmount), Math.sqrt(pAmount), Math.sqrt(pAmount));
     scene.add(pMesh);
@@ -144,99 +166,125 @@ var devour = function (other) {
         if (Math.sqrt(pAmount) > Math.sqrt(other.amount) * 1.1) {
             pSavedAmount += other.amount;
             other.alive = false;
-        } else if (Math.sqrt(other.amount) > Math.sqrt(pAmount) * 1.1) {
-            Math.sqrt(other.amount) += Math.sqrt(pAmount);
-            pPosition.x += 20;
+        } else if (Math.sqrt(other.amount) > Math.sqrt(pAmount) * 1.1 && pAlive == true) {
+            other.amount += pAmount;
+            pAlive = false;
+            pAmount = 9;
+            pPosition = {x: Math.random() * 1000, y: Math.random() * 1000, z:Math.random() * 1000};
         }
     }
 }
 
-var updatePlayer = function () {
-    pRotation.x = camera.rotation.x;
-    pRotation.y = camera.rotation.y;
-    pRotation.z = camera.rotation.z;
 
-    pMesh.rotation.set(pRotation.x, pRotation.y, pRotation.z);
-    pMesh.position.set(pPosition.x, pPosition.y, pPosition.z);
+    var updatePlayer = function () {
+        pRotation.x = camera.rotation.x;
+        pRotation.y = camera.rotation.y;
+        pRotation.z = camera.rotation.z;
 
-    //GROW
-    pAmount += pSavedAmount/30;
-    pSavedAmount -= pSavedAmount/30;
+        pMesh.rotation.set(pRotation.x, pRotation.y, pRotation.z);
+        //pMesh.position.set(pPosition.x, pPosition.y, pPosition.z);
+
+        //GROW
+        pAmount += pSavedAmount / 30;
+        pSavedAmount -= pSavedAmount / 30;
+        pMesh.scale.set(Math.sqrt(pAmount), Math.sqrt(pAmount), Math.sqrt(pAmount));
+
+        controls.minDistance = Math.sqrt(pAmount) * 4;
+        controls.maxDistance = Math.sqrt(pAmount) * 4;
+
+        //ADD TO VERSION AT HOME
+        //pPosition.x += pX / 30;
+        pX -= pX / 30;
+        pMesh.translateZ(pX/30);
+        //pPosition.y += pY / 30;
+        pY -= pY / 30;
+        pMesh.translateY(pY/30);
+        //pPosition.z += pZ / 30;
+        pZ -= pZ / 30;
+        pMesh.translateX(pZ/30);
+        pPosition = pMesh.position;
+        //END
+
+        players.forEach(function (player) {
+            devour(player);
+        })
+        asteroids.forEach(function (asteroid) {
+            devour(asteroid);
+        })
+    }
+
+    var clearPlayers = function () {
+        players.forEach(function (player) {
+            scene.remove(player.mesh);
+            player.mesh.geometry.dispose();
+            player.mesh.material.dispose();
+        })
+    }
+
+    var updateAsteroids = function () {
+        asteroids.forEach(function (asteroid) {
+            if (!asteroid.alive) {
+                scene.remove(asteroid.mesh);
+                asteroid.mesh.geometry.dispose();
+                asteroid.mesh.material.dispose();
+            }
+            asteroid = null;
+        })
+    }
+
+    var updatePlayers = function () {
+        players.forEach(function (player) {
+            //Mesh Creation
+            if (player.alive) {
+                var geometry = new THREE.SphereGeometry(1, 32, 32);
+                var material = new THREE.MeshPhongMaterial({ color: player.color });
+                player.mesh = new THREE.Mesh(geometry, material);
+                player.mesh.position.set(player.position.x, player.position.y, player.position.z);
+                player.mesh.scale.set(Math.sqrt(player.amount), Math.sqrt(player.amount), Math.sqrt(player.amount));
+                scene.add(player.mesh);
+            }
+        })
+    }
+
     pMesh.scale.set(Math.sqrt(pAmount), Math.sqrt(pAmount), Math.sqrt(pAmount));
-
-    pPosition.x += pX/30;
-    pX -= pX/30;
-    pPosition.z += pZ/30;
-    pZ -= pZ/30;
-    pMesh.position.set(pPosition.x,pPosition.y,pPosition.z);
-
-    players.forEach(function (player) {
-        devour(player);
-    })
-}
-
-var clearPlayers = function () {
-    players.forEach(function (player) {
-        scene.remove(player.mesh);
-        player.mesh.geometry.dispose();
-        player.mesh.material.dispose();
-    })
-}
-
-var updatePlayers = function () {
-    players.forEach(function (player) {
-        //Mesh Creation
-        if (player.alive) {
-            var geometry = new THREE.SphereGeometry(1, 32, 32);
-            var material = new THREE.MeshPhongMaterial({ color: player.color });
-            player.mesh = new THREE.Mesh(geometry, material);
-            player.mesh.position.set(player.position.x, player.position.y, player.position.z);
-            player.mesh.scale.set(Math.sqrt(player.amount), Math.sqrt(player.amount), Math.sqrt(player.amount));
-            scene.add(player.mesh);
-        }
-    })
-}
-
-pMesh.scale.set(Math.sqrt(pAmount), Math.sqrt(pAmount), Math.sqrt(pAmount));
-camera.position.set(pPosition.x, pPosition.y + Math.sqrt(pAmount) * 2, pPosition.z);
-updatePlayers();
-
-var updateGame = function () {
-    updatePlayer();
-    clearPlayers();
+    camera.position.set(pPosition.x, pPosition.y + Math.sqrt(pAmount) * 2, pPosition.z);
     updatePlayers();
 
-}
+    var updateGame = function () {
+        updatePlayer();
+        clearPlayers();
+        updatePlayers();
+        updateAsteroids();
+    }
 
-var render = function () {
-    updateGame();
-    requestAnimationFrame(render);
-    renderer.render(scene, camera);
-    controls.target = pMesh.position;
-    controls.update();
-    players[1].position.x += 0.03;
-}
+    var render = function () {
+        updateGame();
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+        controls.target = pMesh.position;
+        controls.update();
+    }
 
 
-//SERVER COMMUNICATION
-var getPlayers = function () {
-    fetch(`${url}/${app.page}/game`).then(function (res) {
-        res.json().then(function (data) {
-            players = data.players
+    //SERVER COMMUNICATION
+    var getPlayers = function () {
+        fetch(`${url}/${app.page}/game`).then(function (res) {
+            res.json().then(function (data) {
+                players = data.players
+            });
         });
-    });
-}
+    }
 
-var sendPlayer = function () {
-    fetch(`${url}/${app.page}/game`, {
-        method: "POST",
-        headers: {
-            "Content-type": "application/json"
-        },
-        body: JSON.stringify({
-            players: players
+    var sendPlayer = function () {
+        fetch(`${url}/${app.page}/game`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                players: players
+            })
         })
-    })
-}
+    }
 
-render();
+    render();
