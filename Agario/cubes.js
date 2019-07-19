9205b771d0e37aaa7a108eeeeaac18cc16a07562
AntoineOMEGA <31717032+AntoineOMEGA@users.noreply.github.com>
2019-07-19 11:06:31 -0600

    Update cubes.js

--- Agario/cubes.js
+++ Agario/cubes.js
@@ -272,6 +272,9 @@ var clearPlayers = function () {
         scene.remove(player.mesh);
         player.mesh.geometry.dispose();
         player.mesh.material.dispose();
+        scene.remove(player.ring);
+        player.ring.geometry.dispose();
+        player.ring.material.dispose();
     })
 }
 
