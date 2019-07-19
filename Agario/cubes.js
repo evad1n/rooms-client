323a565c571bc838db5c65c772af2347bc29c8a2
AntoineOMEGA <31717032+AntoineOMEGA@users.noreply.github.com>
2019-07-19 10:09:12 -0600

    Update cubes.js

--- Agario/cubes.js
+++ Agario/cubes.js
@@ -199,6 +199,7 @@ mtlLoader.load('models/raptor.mtl', function (materials) {
 var createPlayer = function () {
     var geometry = new THREE.SphereGeometry(1, 32, 32);
     var material = new THREE.MeshStandardMaterial({
+        color: 0x000000,
         roughness: 0,
         envMap: scene.background
     });
